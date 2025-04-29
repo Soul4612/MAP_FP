bdad4cb73a7d71c7e021a5068fc00710b5decc4a
Soul4612 <asdg4414@gmail.com>
2025-04-29 20:25:36 +0800

    簡單更新字典(尚未實作內容)

--- app/src/main/java/fcu/iecs/morselearning/DictionaryActivity.java
+++ app/src/main/java/fcu/iecs/morselearning/DictionaryActivity.java
@@ -1,6 +1,9 @@
 package fcu.iecs.morselearning;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DictionaryActivity extends AppCompatActivity {
+    private ImageView iv_back;
+    private ImageView iv_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,23 @@ public class DictionaryActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        iv_back = findViewById(R.id.iv_back);
+        iv_home = findViewById(R.id.iv_home);
+
+        iv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        iv_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
