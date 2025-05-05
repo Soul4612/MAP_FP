fa2255f7b7a5494462b4d42b65efff8abefe90aa
Stephanie <stephanie17aug@gmail.com>
2025-05-05 13:00:49 +0800

    學習模式UI

--- app/src/main/java/fcu/iecs/morselearning/LearningActivity.java
+++ app/src/main/java/fcu/iecs/morselearning/LearningActivity.java
@@ -2,6 +2,8 @@ package fcu.iecs.morselearning;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class LearningActivity extends AppCompatActivity {
     private ImageView ivBack;
     private ImageView ivHome;
+    private Button btnIntro;
+    private Button btnCh1;
+    private Button btnCh2;
+    private Button btnCh3;
+    private Button btnCh4;
+    private Button btnCh5;
+    private Button btnCh6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,13 @@ public class LearningActivity extends AppCompatActivity {
         
         ivBack = findViewById(R.id.iv_back_learning);
         ivHome = findViewById(R.id.iv_home_learning);
+        btnCh1 = findViewById(R.id.btn_ch1);
+        btnCh2 = findViewById(R.id.btn_ch2);
+        btnCh3 = findViewById(R.id.btn_ch3);
+        btnCh4 = findViewById(R.id.btn_ch4);
+        btnCh5 = findViewById(R.id.btn_ch5);
+        btnCh6 = findViewById(R.id.btn_ch6);
+        btnIntro = findViewById(R.id.btn_intro);
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +55,13 @@ public class LearningActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        btnCh1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
